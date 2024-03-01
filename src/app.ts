245e2a2d951cd403dccd9ac39cf0178a76c3f0e4
julio <juliognoatto5@gmail.com>
2024-02-29 23:44:07 -0300

    add errors validations

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import cors from 'cors'
 import helmet from "helmet";
 import "express-async-errors";
 import { router } from "./routes";
+import { handleErrors } from "./middlewares";
 
 export const app: Application = express();
-app.use(cors());
 
+app.use(cors());
 app.use(helmet());
 app.use(json());
 
 app.use("/cars", router)
+
+app.use(handleErrors)
