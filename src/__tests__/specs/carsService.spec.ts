546838ac60308a7e4feb64020624ce2fbd23b5d0
julio <juliognoatto5@gmail.com>
2024-03-07 22:18:05 -0300

    add tests

--- src/__tests__/specs/carsService.spec.ts
+++ src/__tests__/specs/carsService.spec.ts
@@ -1,6 +1,6 @@
 import { CarsService } from "../../services";
 import { CarServiceMock } from "../mocks/carsService.mock";
-import { Car, CarCreate } from "../../interfaces";
+
 
 describe('CarService', () => {
   let carService: CarsService;
@@ -10,7 +10,7 @@ describe('CarService', () => {
   });
 
   it('should create a car', async () => {
-    const car: CarCreate = {
+    const car = {
       name: 'Car name',
       description: 'Car description',
       brand: 'Card brand',
@@ -28,8 +28,15 @@ describe('CarService', () => {
     });
   });
 
-  it('should get a car', async () => {
-    const car = await carService.read();
+  it('should get all cars', async () => {
+    const cars = await carService.read();
+    expect(cars).toBeInstanceOf(Array);
+    expect(cars.length).toBeGreaterThan(0);
+  });
+
+  it('should get a car by id', async () => {
+    const carId = 'fe111d24-1b79-44df-931b-4c9fd5859014';
+    const car = await carService.readById(carId);
     expect(car).toEqual({
       id: 'fe111d24-1b79-44df-931b-4c9fd5859014',
       name: 'Car name',
@@ -40,5 +47,28 @@ describe('CarService', () => {
     });
   });
 
-  // Add tests for updateCar and deleteCar methods
+  it('should update a car', async () => {
+    const carId = 'fe111d24-1b79-44df-931b-4c9fd5859014';
+    const updatedCar = await carService.update(carId, {
+      name: 'Car name updated',
+      description: 'Car description updated',
+      brand: 'Card brand updated',
+      year: 2022,
+      km: 20000
+    });
+    expect(updatedCar).toEqual({
+      id: 'fe111d24-1b79-44df-931b-4c9fd5859014',
+      name: 'Car name updated',
+      description: 'Car description updated',
+      brand: 'Card brand updated',
+      year: 2022,
+      km: 20000
+    });
+  });
+
+  it('should delete a car', async () => {
+    const carId = 'fe111d24-1b79-44df-931b-4c9fd5859014';
+    const deletedCar = await carService.delete(carId);
+    expect(deletedCar).toBeNull();
+  });
 });
